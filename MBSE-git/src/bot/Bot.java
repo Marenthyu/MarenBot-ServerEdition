545ea4c0c9e9a9565e25b3b75777dcbcc166e37a
Marenthyu <petersilie93@live.de>
2015-05-03 14:39:55 +0200

    Working on #4 - Added PricBot to Buildpath and filesystem - Created InstanceManager with Javadoc - First implementation of Bot, not yet completed to be tested (Javadoc to be done)
    #4 still open.

---
+++ MBSE-git/src/bot/Bot.java
@@ -0,0 +1,40 @@
+package bot;
+
+import java.io.IOException;
+
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.PircBot;
+
+public class Bot extends PircBot {
+
+	private String channel;
+
+	public Bot(String Name, String Channel) throws Exception {
+		this.setName(Name);
+		this.setLogin(Name);
+		channel = Channel;
+		try {
+			connect("irc.twitch.tv", 6667,
+					"oauth:ulvex0dv2jk7nz38fd9l6bcqofw3ds");
+			joinChannel(channel);
+		} catch (IOException | IrcException e) {
+			System.err.println("Failed to connect to channel " + channel);
+			throw e;
+
+		}
+		sendMessage(
+				channel,
+				"MarenBot (ServerEdition) has joined this Channel but is not yet ready to rock.");
+	}
+	
+	public String getChannel() {
+		return channel;
+	}
+	
+	public void terminate() {
+		sendMessage(channel,"Terminating Connection to this channel");
+		this.disconnect();
+		this.dispose();
+	}
+
+}
---
+++ MBSE-git/src/bot/InstanceManager.java
@@ -0,0 +1,118 @@
+package bot;
+
+import java.util.ArrayList;
+
+/**
+ * Manager Class for all running instances of {@link Bot}s. <br>
+ * Call {@link #initialize()} before using any other method.
+ * 
+ * @author Peter Fredebold
+ * @see {@link #newInstance} {@link #initialize()}
+ *
+ */
+public class InstanceManager {
+
+	/**
+	 * {@link ArrayList} of all running instances.
+	 */
+	private ArrayList<Bot> instances;
+
+	/**
+	 * Initializes the InstanceManager by creating an empty {@link ArrayList}
+	 * which will be filled with all running instances of {@link Bot}s.
+	 */
+	public void initialize() {
+		instances = new ArrayList<Bot>();
+	}
+
+	/**
+	 * This Method returns an {@link ArrayList} of {@link Bot}s that are
+	 * currently running.
+	 * 
+	 * @return an {@link ArrayList} of {@link Bot}s that are currently running.
+	 */
+	public ArrayList<Bot> getAllInstances() {
+		return instances;
+	}
+
+	/**
+	 * Creates a new instance of a {@link Bot} and adds it to the list of
+	 * running instances while returning if the creation was successful.
+	 * 
+	 * @param channel
+	 *            - the channel to join
+	 * @return <p>
+	 *         <b>true</b> if Instance was successfully created in the specified
+	 *         channel
+	 *         </p>
+	 *         <p>
+	 *         <b>false</b> if another instance in that channel already exists
+	 *         </p>
+	 */
+	public boolean newInstance(String channel) {
+		if (getInstanceByChannel(channel) == null)
+			try {
+				instances.add(new Bot("MarenBot", channel));
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		else {
+			System.err
+					.println("Instance already exists. Please terminate it before creating a new one!");
+
+		}
+		return false;
+	}
+
+	/**
+	 * This Method terminates a running instance of a {@link Bot} in the
+	 * specified channel and returns if the termination was successful.
+	 * 
+	 * @param channel
+	 * @return <p>
+	 *         <b>true</b> if Instance was successfully terminated from the
+	 *         specified channel
+	 *         </p>
+	 *         <p>
+	 *         <b>false</b> if no instance was in that channel
+	 *         </p>
+	 */
+	public boolean terminateInstanceByChannel(String channel) {
+
+		for (Bot b : instances) {
+			if (b.getChannel().equals(channel)) {
+				b.terminate();
+				instances.remove(b);
+				System.out.println("Terminated instance \"" + channel + "\"");
+				return true;
+			}
+
+		}
+		return false;
+	}
+
+	/**
+	 * This Method returns a {@link Bot} object that fits to the specified
+	 * channel
+	 * 
+	 * @param channel
+	 * @return <p>
+	 *         a <b>{@link Bot}</b> object if there was a running instance in
+	 *         the specified channel
+	 *         </p>
+	 *         <p>
+	 *         <b>null</b> if no running instance was found for the specified
+	 *         channel
+	 *         </p>
+	 */
+	public Bot getInstanceByChannel(String channel) {
+		for (Bot b : instances) {
+			if (b.getChannel().equals(channel)) {
+				return b;
+			}
+		}
+		return null;
+	}
+
+}
